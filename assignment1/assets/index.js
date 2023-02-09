3c95a8ae173198e020cbb950449e138c1295d7b9
Имя <prybak497@gmail.com>
2023-02-09 17:48:04 +0300

    .

--- assignment1/assets/index.js
+++ assignment1/assets/index.js
@@ -2,4 +2,34 @@ const taskTextInput=document.querySelector('.text');
 const addButton=document.querySelector('.button-add');
 const container=document.querySelector('.task-list__container');
 const cleanButton=document.querySelector('.button-clean');
+const tasks=[];
 
+addButton.onclick = () => {
+    const inputValue=taskTextInput.value;
+    if (inputValue) {
+        addTask(inputValue);
+        taskTextInput.value="";
+    }
+console.log(tasks);
+}
+
+function createTask(text) {
+    const task = {
+        text: text,
+        isComplete: false
+    }
+    tasks.push(task);
+}
+
+// function addTask
+tasks.forEach((item) => {
+    const containerCheckbox=document.createElement('input');
+    containerCheckbox.className='container__checkbox';
+    containerCheckbox.type="checkbox";
+    container.append(containerCheckbox);
+
+    const containerContent=document.createElement('p');
+    containerContent.className='container__content';
+    container.append(containerContent);
+    containerContent.textContent=item.text;
+});
