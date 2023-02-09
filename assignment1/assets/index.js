4e42f09e491b18c5fd8ec91f64fd8cd78b62ac2b
Имя <prybak497@gmail.com>
2023-02-09 14:45:02 +0300

    .

---
+++ assignment1/assets/index.js
@@ -0,0 +1,5 @@
+const taskTextInput=document.querySelector('.text');
+const addButton=document.querySelector('.button-add');
+const container=document.querySelector('.task-list__container');
+const cleanButton=document.querySelector('.button-clean');
+
